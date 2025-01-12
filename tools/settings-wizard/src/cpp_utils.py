5efe24bde3781cd6a4b366607018b310c59450da
Maciej Manna <maciejmanna@gmail.com>
2025-01-12 02:14:12 +0100

    fixed missing endlines in generated files

--- tools/settings-wizard/src/cpp_utils.py
+++ tools/settings-wizard/src/cpp_utils.py
@@ -6,12 +6,13 @@ def _get_line(i: int) -> str:
 def add_include(ls: list[str], i: int, header: str, is_quoted: bool = True) -> None:
     l = _get_line(i) + '#include '
     l += f'"{header}"' if is_quoted else f'<{header}>'
+    l += '\n'
     ls.append(l)
 
 def add_blank(ls: list[str], count: int = 1):
     for _ in range(count):
-        ls.append('')
+        ls.append('\n')
 
 def add_block_comment(ls: list[str], i: int, *args) -> None:
     for arg in args:
-        ls.append(_get_line(i) + '// ' + str(arg))
+        ls.append((_get_line(i) + '// ' + str(arg)).rstrip() + '\n')
